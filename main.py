f4d3d4fe5bc93fd5bd6800fabab700cf125d9e8b
simsar <1muhammetkayak@gmail.com>
2022-07-13 16:01:38 +0300

    mas

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 from PIL import Image
 import glob
 import os
-img_dir = "C:/Users/Muhammet.ORHAN/Desktop/Python/İmageResizer/product/"
+img_dir = "C:/Users/Muhammet.ORHAN/Desktop/Python/İmageResizer/GWD/"
 dirs = os.listdir(img_dir)
-
 
 
 def resize_bulk_images():
     for img in dirs:
         if os.path.isfile(img_dir + img):
             im = Image.open(img_dir + img)
-            imageWidth =int(round(im.size[0]*0.4)) 
-            image_height =int(round(im.size[1]*0.4)) 
-            
-            print(int(im.size[0])-imageWidth,int(im.size[1])-image_height)
-            
-       
+            imageWidth = int(round(im.size[0]*0.5))
+            image_height = int(round(im.size[1]*0.5))
+
+            print(int(im.size[0])-imageWidth, int(im.size[1])-image_height)
+
             f, e = os.path.splitext(img_dir + img)
-            imResize = im.resize((imageWidth,imageWidth), Image.ANTIALIAS)
-            imResize.save(f + '_structured.webp', quality=100)
-            
-            
+            imResize = im.resize((imageWidth, image_height), Image.ANTIALIAS)
+            imResize.save(f + '_resized.png', quality=100)
+
+
 print('Bulk images re-structure started...')
 
 
